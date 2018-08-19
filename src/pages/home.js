3747c9112f416970fcd01f3ad6ed1e3acb911f14
Hamilton Chevez <hchev001@fiu.edu>
2018-08-19 19:55:47 -0400

    working on creating the navigation bar of the home page

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../App.css";
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <a href="#Designers">Designers</a>
+        <a href="#Developers">Developers</a>
+        <a href="#Community">Community</a>
+        <a href="#Hire">Hire</a>
+        <a href="#LogIn">Log In</a>
+      </div>
+    );
+  }
+}
