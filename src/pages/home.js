a4740082c24427837077129f0bbdb11ad58b2afd
Hamilton Chevez <hchev001@fiu.edu>
2018-08-19 20:30:56 -0400

    did a rough top menu and added some of the basic content of the home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,59 @@
 import React, { Component } from "react";
 import "../App.css";
 
+export class NavBar extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <div className="navbar-logo">MEMERAMA</div>
+        <div className="navbar-links">
+          <a className="link" href="#Designers">
+            Designers
+          </a>
+          <a className="link" href="#Developers">
+            Developers
+          </a>
+          <a className="link" href="#Community">
+            Community
+          </a>
+          <a className="link" href="#Hire">
+            Hire
+          </a>
+          <a className="link" href="#LogIn">
+            Log In
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export class MainSection extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Community Giving Opportunity</h1>
+        <img />
+        <p>
+          Memerama is the largest community of accomplished designers and
+          developers giving each other work opportunities and making thousands
+          in revenue.
+        </p>
+        <button />
+        <button />
+        <h3>FREE for first 3 months</h3>
+        <h3>No Credit Card Needed</h3>
+      </div>
+    );
+  }
+}
+
 export class Home extends Component {
   render() {
     return (
       <div>
-        <a href="#Designers">Designers</a>
-        <a href="#Developers">Developers</a>
-        <a href="#Community">Community</a>
-        <a href="#Hire">Hire</a>
-        <a href="#LogIn">Log In</a>
+        <NavBar />
+        <MainSection />
       </div>
     );
   }
